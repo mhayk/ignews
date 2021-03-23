2e65b8ef986a0c0a80c900de01216d05368b6687
Mhayk Whandson <hi@mhayk.com>
2021-03-23 23:41:29 +0000

    The component SubscribeButton has been imported in home.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from './home.module.scss';
 
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for £9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
