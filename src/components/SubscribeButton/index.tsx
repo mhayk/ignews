fa7b2d520c688c2e179be857a5933018dad99dc1
Mhayk Whandson <hi@mhayk.com>
2021-03-29 19:57:08 +0100

    The redirection to stripe web page has been configured.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,44 @@
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+// The security places to use key
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession()
+
+    async function handleSubscribe() {
+        if (!session) {
+            signIn('github')
+            return;
+        }
+
+        // create checkout session
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+        } catch (err) {
+            alert(err.message)
+        }
+    }
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
